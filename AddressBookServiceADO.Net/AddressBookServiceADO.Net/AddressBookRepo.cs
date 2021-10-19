29d1661efe5e00e7c2ae1e2cd83642a195e8b374
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-10-19 19:52:25 +0530

    Create Connection From Database

---
+++ AddressBookServiceADO.Net/AddressBookServiceADO.Net/AddressBookRepo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AddressBookServiceADO.Net
+{
+    public class AddressBookRepo
+    {
+        /// <summary>
+        /// Database server store in string
+        /// </summary>
+        public static string connectionString = @"Data Source=DESKTOP-AB32E02;Initial Catalog=AddressBookService;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        /// <summary>
+        /// check database connection methods
+        /// </summary>
+        public void DBconnection()
+        {
+            //open server
+            connection.Open();
+            if (connection.State == ConnectionState.Open)
+            {
+                Console.WriteLine("Database connection is succesfull & is opened !!");
+            }
+            else
+            {
+                Console.WriteLine("Connection is not established !!");
+            }
+            connection.Close();
+        }
+    }
+}
---
+++ AddressBookServiceADO.Net/AddressBookServiceADO.Net/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AddressBookServiceADO.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to address book problem ADO.net");
+            /// Creating an object of AddressBookRepo class
+            AddressBookRepo addressBookRepo = new AddressBookRepo();
+            /// Calling the GetAllEntries method from AddressBookRepo class
+            addressBookRepo.DBconnection();
+        }
+    }
+}
